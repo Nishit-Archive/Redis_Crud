facf1f12bd15a7409c61004375918e2a7f4a65ff
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-09-24 14:31:08 +0530

    Update route.js

--- route.js
+++ route.js
@@ -1,46 +1,48 @@
-module.exports = (app, client,res,req) => {
-    //app get route
-    app.get("/", async (req, res) => {
-      res.send("Hello World to Redis Now time to learn Redis");
-    });
-  
-    app.post("/createuser", async (req, res) => {
-      const { username, email, password } = req.body;
-      if (!username || !email || !password) {
-        return res.status(400).json({ error: "Please fill all the fields" });
-      }
-  
-      try {
-        await client.connect();
-  
-        const allUsers = await client.get("allUsers");
-        let userArray = [];
-        if (allUsers) {
-          userArray = JSON.parse(allUsers);
-          const isUsernameTaken = userArray.some(
-            (user) => user.username === username
-          );
-          if (isUsernameTaken) {
-            return res.status(400).json({ error: "Username already taken" });
-          }
-  
-          const newUser = {
-            username,
-            email,
-            password,
-            createdAt: new Date().toISOString(),
-          };
-  
-          userArray.push(newUser);
-          await client.set("allUsers", JSON.stringify(userArray));
-          res.status(200).json({ message: "User Created Successfully", newUser });
+module.exports = (app, client) => {
+  //app get route
+  app.get("/", async (req, res) => {
+    res.send("Hello World to Redis Now time to learn Redis");
+  });
+
+  app.post("/createuser", async (req, res) => {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({ error: "Please fill all the fields" });
+    }
+
+    try {
+      await client.connect();
+
+      const allUsers = await client.get("allUsers");
+      let usersArray = [];
+      if (allUsers) {
+        usersArray = JSON.parse(allUsers);
+        const isUsernametaken = usersArray.some(
+          (user) => user.username === username,
+        );
+        if (isUsernametaken) {
+          res.status(400).json({ error: "username is already taken" });
+          return;
         }
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
-      } finally {
-        client.disconnect();
       }
-    });
-  };
-  
+
+      const newUser = {
+        username,
+        email,
+        password,
+        createdAt: new Date().toISOString(),
+      };
+
+      usersArray.push(newUser);
+
+      await client.set("allUsers", JSON.stringify(usersArray));
+      res.json({ message: "User created successfully", user: newUser });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    } finally {
+      client.disconnect();
+    }
+  });
+};
